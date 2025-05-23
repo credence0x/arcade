9229588d92e995ab41ce904d65bc3f6d5924aa35
nrmnqdds <quddusnuriman@gmail.com>
2025-05-23 16:41:02 +0800

    fix: update arcade icon and gap

--- client/src/components/connection/user.tsx
+++ client/src/components/connection/user.tsx
@@ -63,7 +63,7 @@ export function User() {
   };
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-3">
       <Button
         variant="secondary"
         className="bg-spacer-100 px-3 py-2.5"
